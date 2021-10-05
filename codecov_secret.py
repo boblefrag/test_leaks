9e632592fe61cd611a128010668bd1fadea9d1ab
Yohann Gabory <yohann.gabory@gitguardian.com>
2021-10-05 15:52:04 +0200

    another leak...

--- codecov_secret.py
+++ codecov_secret.py
@@ -1,1 +1,6 @@
 CODECOV_TOKEN="835a4597-5614-40f5-803c-093464aa2a74"
+
+
+idpolicies=$(curl -X GET 'https://api.newrelic.com/v2/alerts_policies.json' -H X-Api-Key: b862dec293faae411c31b3160819c591 -s -G |jq '.policies[] .id')
+
+apikey: b862dec293faae411c31b3160819c591
