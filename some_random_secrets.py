b03124c8ac820e5e88d90929b8ebb8fb4a53613d
Yohann Gabory <yohann.gabory@gitguardian.com>
2021-10-05 15:34:11 +0200

    more

--- some_random_secrets.py
+++ some_random_secrets.py
@@ -1,3 +1,21 @@
 AIzaSyBGz7BuujLB_bUo2Pcfk0Tk__Cnae4O7te
 https://api.airtable.com/v0/appddddedddcheval/poney
 brw=brwuDrlBAuNlnb5yz; path=/; expires=Tue, 29 Jun 2021 17:39:48 GMT; domain=.airtable.com;
+
+
+
+
+    body: null
+    headers:
+      Accept:
+      - '*/*'
+      Accept-Encoding:
+      - gzip, deflate
+      Connection:
+      - keep-alive
+      Content-Type:
+      - application/json
+      User-Agent:
+      - python-requests/2.23.0
+    method: HEAD
+    uri: https://api.airtable.com/v0/appddddedddcheval/poney
