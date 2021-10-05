4a7b6f8ec11335513ed8d03c1cb1b990e094250d
Yohann Gabory <yohann.gabory@gitguardian.com>
2021-10-05 15:36:00 +0200

    more

--- some_random_secrets.py
+++ some_random_secrets.py
@@ -19,3 +19,9 @@ brw=brwuDrlBAuNlnb5yz; path=/; expires=Tue, 29 Jun 2021 17:39:48 GMT; domain=.ai
       - python-requests/2.23.0
     method: HEAD
     uri: https://api.airtable.com/v0/appddddedddcheval/poney
+
+
+
+    const ALGOLIA_APP_ID = 'OH29RAPN3N';
+    const ALGOLIA_ADMIN_KEY = 'b782df173961404a169945f8079362a3';
+    const ALGOLIA_SEARCH_KEY = '46aee733e12b4f08ee8f1ba9c4bc6380';
