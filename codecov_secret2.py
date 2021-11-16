faccda25dc2cc0166c3668cac6694a704b1898cd
John Doe <yohann.gabory+restricted@gitguardian.com>
2021-11-16 15:51:28 +0100

    oups

---
+++ codecov_secret2.py
@@ -0,0 +1,8 @@
+CODECOV_TOKEN="835a4597-5614-40f5-803c-093464aa2a74"
+
+
+idpolicies=$(curl -X GET 'https://api.newrelic.com/v2/alerts_policies.json' -H X-Api-Key: b862dec293faae411c31b3160819c591 -s -G |jq '.policies[] .id')
+apikey: b862dec293faae411c31b3160819c591
+
+
+curl "https://gitlab.example.com/api/v4/projects?private_token="bfaz8yjCU-u9kE1VmSjS"
